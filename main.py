f02f2affb061431b48daa85fa1f69c1590b11897
Avirup Mondal <avirupmondal1994@gmail.com>
2026-03-20 11:02:34 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("logging is implemented")
+DATA_INGESTION = "Data Ingestion stage"
+
+def main():
+    ## Initiating the data ingestion training pipeline
+    try:
+        logger.info(f"stage {DATA_INGESTION} initiated")
+        data_ingestion_pipeline=DataIngestionTrainingPipeline()
+        data_ingestion_pipeline.initiate_data_ingestion()
+        logger.info(f"Stage {DATA_INGESTION} Completed")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+if __name__=="__main__":
+    main()
