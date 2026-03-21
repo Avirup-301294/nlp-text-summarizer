3d354f26e60badb6ef80dbb68644d527b3c37571
Avirup Mondal <avirupmondal1994@gmail.com>
2026-03-21 17:43:21 +0530

    model evaluation completed

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.textSummarizer.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.textSummarizer.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
 DATA_INGESTION = "Data Ingestion stage"
 DATA_TRANSFORMATION = "Data Transformation stage"
 MODEL_TRAINER = "Model Trainer stage"
+MODEL_EVALUATION = "Model Evaluation stage"
 
 def main():
     ## Initiating the data ingestion training pipeline
@@ -36,6 +38,16 @@ def main():
         model_trainer_pipeline=ModelTrainerTrainingPipeline()
         model_trainer_pipeline.initiate_model_trainer()
         logger.info(f"Stage {MODEL_TRAINER} Completed")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    ## Initiating the model evaluation component
+    try:
+        logger.info(f"stage {MODEL_EVALUATION} initiated")
+        model_evaluation_pipeline=ModelEvaluationTrainingPipeline()
+        model_evaluation_pipeline.initiate_model_evaluation()
+        logger.info(f"Stage {MODEL_EVALUATION} Completed")
     except Exception as e:
         logger.exception(e)
         raise e
